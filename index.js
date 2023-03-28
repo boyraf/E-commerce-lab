6e89032f34ae3f91eeeb8fb9b30a91ca697d7d14
boyraf <raphael.kymotho@gmail.com>
2023-03-28 14:40:45 +0300

    done3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Fetch the list of products from the server and display them in the UI
 fetch("http://localhost:3000/products")
   .then(function (response) {
     return response.json();
@@ -6,15 +7,15 @@ fetch("http://localhost:3000/products")
     const characters = document.querySelector('.details');
 
     data.forEach(function (product) {
+      // Create a new card for each product and add it to the UI
       const card = document.createElement('div');
       card.classList.add('card');
       card.innerHTML = `
-        
-      <img src="${product.image}" alt="Product Image" style="width:50%; height:50%">
-      <div class="container">
-        <h3><b>${product.name}</b></h3>
-        <p>${product.description}</p>
-        <p>${product.price}</p>
+        <img src="${product.image}" alt="Product Image" style="width:50%; height:50%">
+        <div class="container">
+          <h3><b>${product.name}</b></h3>
+          <p>${product.description}</p>
+          <p>${product.price}</p>
           <div class="card-actions">
             <button class="edit-button" style="background-color: #4CAF50; color: white; font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; padding: 8px 16px; border: none; border-radius: 4px; cursor: pointer;">Edit</button>
             <button class="delete-button" style="background-color: #f44336; color: white; font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; padding: 8px 16px; border: none; border-radius: 4px; cursor: pointer;">Delete</button>
@@ -26,42 +27,48 @@ fetch("http://localhost:3000/products")
       // Add event listeners to the buttons
       const editButton = card.querySelector('.edit-button');
       const deleteButton = card.querySelector('.delete-button');
+      
+      deleteButton.addEventListener('click',() =>{
+        card.remove()
+        deleteProduct(product.id)
+      });
+      
       editButton.addEventListener('click', () => {
         // Handle edit action
         console.log('Edit clicked for product', product.id);
       });
-      deleteButton.addEventListener('click', () => {
-        // Handle delete action
-        console.log('Delete clicked for product', product.id);
-      });
     });
   });
-  // Get the reference to the form and attach an event listener for form submit
-  const form = document.querySelector('#productForm');
-  form.addEventListener('submit', handleSubmit);
-  // Define the function that will be called when the form is submitted
-  function handleSubmit(e) {
-    e.preventDefault();
-    let productObj = {
-      name: e.target.name.value,
-      image: e.target.image_url.value,
-      description: e.target.description.value,
-      price: e.target.price.value // Fixed typo here, should be price
-    }
 
-    console.log(productObj)
-    addProduct(productObj);
+// Get the reference to the form and attach an event listener for form submit
+const form = document.querySelector('#productForm');
+form.addEventListener('submit', handleSubmit);
+
+// Define the function that will be called when the form is submitted
+function handleSubmit(e) {
+  e.preventDefault();
+  let productObj = {
+    name: e.target.name.value,
+    image: e.target.image_url.value,
+    description: e.target.description.value,
+    price: e.target.price.value // Fixed typo here, should be price
   }
-  // Define the function that will add a new product to the server
-  function addProduct(productObj) {
-    fetch('http://localhost:3000/products', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(productObj)
-    })
-    .then(res => res.json())
+
+  console.log(productObj)
+  addProduct(productObj);
+}
+
+// Define the function that will add a new product to the server
+function addProduct(productObj) {
+  fetch('http://localhost:3000/products', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(productObj)
+  })
+  .then(res => res.json
+())
     .then(product => console.log(product))
     .catch(err => console.error(err)); // Add error handling
   }
